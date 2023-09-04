7261219b6e0588f921c543ad6242741a2be94965
Luca Balado <lucabalado@gmail.com>
2023-09-04 14:57:11 -0300

    completar tablas

---
+++ post.sql
@@ -0,0 +1,4 @@
+INSERT INTO post(author_id, title, body)
+VALUES ("1", "homelochino", "soy homelo pelo homelo chino"),
+	("3", "yyyy", "ñkpolpl3.2"),
+	("2", "kkkk", "voasdcascd")
---
+++ user.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (username, password)
+VALUES ("homelochino", " 123"),
+	("nashe", "111"),
+	("Yo", "912");
